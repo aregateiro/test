51e7d488731083f3d9f2b1f380c99d9a0c11566d
aregateiro <RegateiroAndre@gmail.com>
2024-01-15 14:28:04 -0800

    Update allocation.py

--- allocation.py
+++ allocation.py
@@ -26,49 +26,44 @@ def allocate_work(staff_licenses, state_workloads):
     return staff_allocations, unallocated_work
 
 
-import streamlit as st
-
 # Streamlit interface
 st.title("Work Allocation App")
 
-# List of states for the dropdown
-states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 
-          'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 
-          'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 
-          'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 
-          'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 
-          'West Virginia', 'Wisconsin', 'Wyoming']
+# Define columns for layout
+col1, col2 = st.columns(2)
 
-# Dynamic input for staff licenses
-st.write("Enter Staff Licenses:")
-staff_licenses = {}
-staff_num = st.number_input('Number of staff members', min_value=1, max_value=20, value=5)
-for i in range(staff_num):
-    staff_name = st.text_input(f'Staff member {i+1} name', value=f'Staff {i+1}', key=f'staff_{i}_name')
-    licenses = st.multiselect(f'Select states for {staff_name}', states, key=f'staff_{i}_licenses')
-    staff_licenses[staff_name] = licenses
+# Worker's Licensing Information
+with col1:
+    st.subheader("Staff Licensing")
+    staff_licenses = {}
+    staff_num = st.number_input('Number of Staff Members', min_value=1, max_value=20, value=5)
+    for i in range(staff_num):
+        staff_name = st.text_input(f'Staff {i+1} Name', value=f'Staff {i+1}', key=f'staff_{i}_name')
+        licenses = st.multiselect(f'{staff_name} - Licensed States', states, key=f'staff_{i}_licenses')
+        staff_licenses[staff_name] = licenses
 
-# Dynamic input for state workloads
-st.write("Enter State Workloads:")
-state_workloads = {}
-state_num = st.number_input('Number of states to enter workloads for', min_value=1, max_value=len(states), value=3)
-for i in range(state_num):
-    state = st.selectbox(f'State {i+1}', states, key=f'state_{i}')
-    hours = st.number_input(f'Hours needed for {state}', min_value=0, max_value=1000, value=40, key=f'hours_{i}')
-    state_workloads[state] = hours
+# States' Workload Information
+with col2:
+    st.subheader("State Workloads")
+    state_workloads = {}
+    state_num = st.number_input('Number of States with Workloads', min_value=1, max_value=len(states), value=3)
+    for i in range(state_num):
+        state = st.selectbox(f'State {i+1}', states, key=f'state_{i}')
+        hours = st.number_input(f'{state} - Workload Hours', min_value=0, max_value=1000, value=40, key=f'hours_{i}')
+        state_workloads[state] = hours
 
+# Allocate work and display results
 if st.button('Allocate Work'):
     allocations, unallocated_work = allocate_work(staff_licenses, state_workloads)
     
-    # Displaying the results in a table
-    st.write("Allocation Results:")
+    # Results of Work Allocation
+    st.subheader("Allocation Results")
     df = pd.DataFrame([(staff, state, hours) for staff, states in allocations.items() for state, hours in states.items()],
                       columns=['Staff', 'State', 'Allocated Hours'])
     st.table(df)
 
-    # Display warning if there is unallocated work
+    # Warning for Unallocated Work
     if unallocated_work:
-        st.warning("Warning: Not all work could be allocated due to insufficient or unavailable staff. Unallocated work:")
+        st.warning("Warning: Not all work could be allocated. Unallocated work details:")
         for state, hours in unallocated_work.items():
             st.write(f"{state}: {hours} hours")
-
